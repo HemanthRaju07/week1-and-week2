460f70bd47045c17cc4cd18759bd2a68c49cf1fd
HemanthRaju07 <hemanthrajukonduru07@gmail.com>
2026-04-21 14:36:01 +0530

    coded to level2-3

---
+++ SquareSide.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double perimeter;
+
+        
+        System.out.print("Enter the perimeter of the square: ");
+        perimeter = sc.nextDouble();
+
+        
+        double side = perimeter / 4;
+
+        
+        System.out.printf("The length of the side is %.2f whose perimeter is %.2f", side, perimeter);
+
+        sc.close();
+    }
+}
